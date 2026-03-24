14d21e7bab2c026042812bd8f0705c9d0ff66fd2
Yigit Durak <yigtdurak@gmail.com>
2026-03-24 07:58:38 +0300

    popup added

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
+let secCon = document.getElementById('container');
+let sec3 = document.getElementById('body');
+let sec = document.getElementById('popup');
+function setOpc(select,value){
+    return select.style.opacity = value;
+}
+function setWtdh(slct,val){
+    return slct.style.width = val;
+}
+function setDsply(sl,vl){
+    return sl.style.display = vl;
+}
+function setPvnt(s,v){
+    return s.style.pointerEvents = v;
+}
+function setOvr(se,va){
+    return se.style.overflowY = va;
+}
 function on_mouse(id) {
     let select = document.getElementById(id);
-    select.style.width = "100%";
+    setWtdh(select,'100%');
 }
 function out_mouse(id){
     let select = document.getElementById(id);
-    select.style.width = "0";
+    setWtdh(select,'0');
+}
+window.addEventListener('load', ()=> {
+    setDsply(sec,'block');
+    setOpc(secCon,'0.4');
+    setPvnt(secCon,'none');
+    setOvr(sec3,'hidden');
+});
+function closer(){
+    setDsply(sec,'none');
+    setOpc(secCon,'1');
+    setPvnt(secCon,'auto');
+    setOvr(sec3,'auto');
 }
